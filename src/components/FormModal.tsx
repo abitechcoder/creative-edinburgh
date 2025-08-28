d32aadba2c3790a90c0809ae4670d82200d6eb79
Oluwabi Shefiu <techplanet49@gmail.com>
2025-08-28 19:47:08 +0100

    Add delete to directory

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -179,8 +179,7 @@ const FormModal = ({
       <form action={formAction} className="p-4 flex flex-col gap-4">
         <input type="text | number" name="id" value={id} hidden />
         <span className="text-center font-medium">
-          {id} All data will be lost. Are you sure you want to delete this{" "}
-          {table}?
+          All data will be lost. Are you sure you want to delete this {table}?
         </span>
         <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
           Delete
