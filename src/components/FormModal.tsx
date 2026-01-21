ff9c6985ea9cfeacb02ac825a059360841df0d68
Tek-Planet <oluwabishefiu@gmail.com>
2026-01-21 21:24:59 +0000

    added list business to footer

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -211,7 +211,6 @@ const FormModal = ({
       )}
       {open && (
         <div className="fixed inset-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
->
           <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
             <Form />
             <div
